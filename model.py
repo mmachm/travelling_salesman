4dbf54433073ef45b9d2533588da8897405df007
Your Name <you@example.com>
2024-01-07 19:02:43 +0100

    Fix initialization issues and constraint issues
    * commented code still included

--- model.py
+++ model.py
@@ -31,12 +31,14 @@ model.dates = Set(initialize=dates, doc="Dates")
 model.profits = Param(model.cities, model.dates, initialize=profits_data, doc="Profits")
 model.transportation_costs = Param(model.cities, model.cities, initialize=costs, doc="Transportation")
 # Assuming start in the first city, in the case London
-#model.initial_position_vector = Param(model.cities, initialize=np.array([1] + [0] * (len(model.cities)-1)))
+# model.initial_position_vector = Param(model.cities, initialize=np.array([1] + [0] * (len(model.cities)-1)))
 
 # Variables
 
-model.concerts_played = Var(model.cities, model.dates, bounds=(0, 1), doc="Attended concerts", domain=pyo.Binary)
-model.flights_taken = Var(model.cities, model.cities, model.dates, bounds=(0, 1), doc="Flights taken", domain=pyo.Binary)
+model.concerts_played = Var(model.cities, model.dates, initialize=0, bounds=(0, 1), doc="Attended concerts", domain=pyo.Binary)
+model.flights_taken = Var(model.cities, model.cities, model.dates, initialize=0, bounds=(0, 1), doc="Flights taken",
+                          domain=pyo.Binary)
+
 
 def init_constraint_rule(model, city_A, city_B, date):
     if city_A == city_B == "London":
@@ -45,86 +47,83 @@ def init_constraint_rule(model, city_A, city_B, date):
         value = 0
     return model.flights_taken[city_A, city_B, date] == value
 
-#model.init_constraint = Constraint(model.cities, model.cities, model.dates, rule=init_constraint_rule)
+
+# model.init_constraint = Constraint(model.cities, model.cities, model.dates, rule=init_constraint_rule)
 
 # TODO cannot fly and play on the same day
 # TODO cannot have more than x concerts in a single country
 
-def satisfy_cross_discreteness(model, day):
+def satisfy_non_multitasking(model, day):
     return (
-            sum(model.concerts_played[city, day] for city in model.cities) +
-            sum(model.flights_taken[city_A, city_B, day] for city_A in model.cities for city_B in model.cities)
-            ) <= 1
+                   sum(model.concerts_played[city, day] for city in model.cities) +
+                   sum(model.flights_taken[city_A, city_B, day] for city_A in model.cities for city_B in model.cities)
+           ) <= 1
+
 
 def satisfy_non_teleportation(model):
-    flights_taken = np.array(
-        [
-            [
-                [
-                    model.flights_taken[city_A, city_B, date]
-                    for date in model.dates
-                ]
-                for city_B in model.cities
-            ]
-            for city_A in model.cities
-        ]
-    )
-    concerts_played = np.array(
-        [
-            [
-                model.concerts_played[city, date]
-                for date in model.dates
-            ]
-            for city in model.cities
-        ]
-    )
-
-    all_ones_vector = np.array([1] * len(model.cities))
-    current_position_vector = np.array([1] + [0] * (len(model.cities)-1))
-    total_teleportations = 0
-    for date_num, _ in enumerate(model.dates):
+    current_position_vector = {city: 1 if city == "London" else 0 for city in model.cities}
+    total_teleportations_list = []
+    for date in model.dates:
         # the following teleports are basically playing in another city
-        try:
-            total_teleportations += Expression(
-                rule=np.dot(all_ones_vector - current_position_vector, concerts_played[:, date_num])
+        #for city in model.cities:
+        #    print((1 - current_position_vector[city]) * model.concerts_played[city, date])
+        #print(            Expression(
+        #        rule=lambda m:  # total_teleportations +
+        #        sum(
+        #            (1 - current_position_vector[city]) * model.concerts_played[city, date] for city in
+        #            model.cities
+        #        )
+        #    ))
+        total_teleportations_list.append(
+            #Expression(
+            #rule=lambda m:  # total_teleportations +
+            sum(
+                (1 - current_position_vector[city]) * model.concerts_played[city, date] for city in
+                model.cities
             )
-        except Exception as ex:
-            raise
+            #)
+        )
         # the following teleports are basically the same as flying from another airport
         # it would lead to living in two cities at the same time, then switching back and forth without having to fly.
-        total_teleportations += np.sum(
-            np.dot(
-                all_ones_vector - current_position_vector,
-                flights_taken[:, :, date_num]
-            )
-        )
-        current_position_vector = Expression(
-            expr=current_position_vector +
-                np.dot(
+        total_teleportations_list.append(
+            #Expression(
+            #    rule=lambda m:  # total_teleportations +
+                sum(
                     (
-                        np.transpose(flights_taken[:, :, date_num]) - flights_taken[:, :, date_num]
-                    ),
-                all_ones_vector,
-            )
+                        (1 - current_position_vector[city_A]) * model.flights_taken[city_A, city_B]
+                        for city_B in model.cities
+                        for city_A in model.cities
+                    )
+                )
+            #)
         )
+        for city_A in model.cities:
+            current_position_vector[city_A] = Expression(
+                expr=current_position_vector[city_A] + sum(
+                    (model.flights_taken[city_B, city_A, date] - model.flights_taken[city_A, city_B, date]) for city_B
+                    in model.cities
+                )
+            )
 
-    return total_teleportations == 0
+    return sum(total_teleportations_list) == 0
 
 
 def total_profit(model):
-    revenue = sum(model.concerts_played[city,date] * model.profits[city,date] for city in model.cities for date in model.dates)
+    revenue = sum(
+        model.concerts_played[city, date] * model.profits[city, date] for city in model.cities for date in model.dates)
     cost = sum(
-        model.flights_taken[city_A, city_B, date] * model.transportation_costs[city_A, city_B, date]
+        model.flights_taken[city_A, city_B, date] * model.transportation_costs[city_A, city_B]
         for city_A in model.cities for city_B in model.cities for date in model.dates
     )
     return revenue - cost
 
-#model.init_constraint = Constraint(model.cities, model.cities, model.dates, rule=init_constraint_rule)
-#model.non_teleportation = Constraint(model.cities, model.cities, model.dates, rule=satisfy_non_teleportation_test, doc='Non-teleportation')
-model.non_teleportation = Constraint(rule=satisfy_non_teleportation, doc='Non-teleportation')
-model.discreteness = Constraint(model.dates, rule=satisfy_cross_discreteness)
 
+# model.init_constraint = Constraint(model.cities, model.cities, model.dates, rule=init_constraint_rule)
+# model.non_teleportation = Constraint(model.cities, model.cities, model.dates, rule=satisfy_non_teleportation_test, doc='Non-teleportation')
+model.non_teleportation = Constraint(rule=satisfy_non_teleportation, doc='Non-teleportation')
+model.discreteness = Constraint(model.dates, rule=satisfy_non_multitasking)
 model.objective = Objective(rule=total_profit, sense=maximize)
+
 
 def pyomo_postprocess(options=None, instance=None, results=None):
     model.concerts_played.display()
@@ -133,6 +132,7 @@ def pyomo_postprocess(options=None, instance=None, results=None):
 from pyomo.opt import SolverFactory
 import pyomo.environ
 
+# model.concerts_played.pprint()
 opt = SolverFactory("glpk")
 results = opt.solve(model)
 # sends results to stdout
